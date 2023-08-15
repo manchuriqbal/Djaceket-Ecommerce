2c29eeb94560069ea9f695f9bd8d1257f7e359d0
manchur <manchur.fiverr@gmail.com>
2023-08-15 22:40:40 +0600

    create simple front page(leates product)

--- vue_djacket/src/views/HomeView.vue
+++ vue_djacket/src/views/HomeView.vue
@@ -1,13 +1,88 @@
 <template>
- 
+  <div class="home">
+    <section class="hero is-medium is-dark mb-d">
+      <div class="hero-body has-text-centered">
+        <p class="title mb-6">
+          Welcome to Djacket
+        </p>
+        <p class="subtitle">
+          The best jacket store online
+        </p>
+      </div>
+    </section>
+
+    <div class="columns is-multiline">
+      <div class="column is-12">
+        <h2 class="is-size-2 has-text-centered"> Leatest Products</h2>
+      </div>
+
+        <div class="column is-3" v-for="product in LeatestProducts" :key="product.id">
+          <div class="box">
+              <figure class="image mb-4 product-image">
+                  <img :src="product.get_thumbnail" alt="Product Image">
+              </figure>
+              <h3 class="is-size-4">{{ product.name }}</h3>
+              <p class="is-size-6 has-text-grey">${{ product.price }}</p>
+              View Product
+          </div>
+        </div>
+
+
+    </div>
+    
+  </div>
 </template>
 
 <script>
-
+import axios from 'axios'
 
 export default {
   name: 'HomeView',
-  components: {
-  }
+  data() {
+    return {
+      LeatestProducts: []
+    }
+  },
+  mounted() {
+    this.getLeatestProducts()
+  },
+  methods: {
+    getLeatestProducts () {
+      axios
+        .get('/api/v1/leatest-products/')
+        .then(response => {
+          this.LeatestProducts = response.data
+        })
+        .catch(error => {
+          console.group(error)
+        })
+    }
+  },
 }
 </script>
+
+<style scoped>
+  .image{
+    margin-top: -1.25rem;
+    margin-left: -1.25rem;
+    margin-right: -1.25rem;
+  }
+  .product-image {
+    width: 100%;
+    height: 0;
+    padding-bottom: 100%; /* 1:1 aspect ratio */
+    position: relative;
+    overflow: hidden;
+}
+
+.product-image img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover; /* Maintain aspect ratio and cover the container */
+}
+
+
+</style>
