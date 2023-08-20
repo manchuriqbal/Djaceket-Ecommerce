787f5f07902301eb94769648b9570984871de1a8
manchur <manchur.fiverr@gmail.com>
2023-08-20 12:51:39 +0600

    create my account page

--- vue_djacket/src/router/index.js
+++ vue_djacket/src/router/index.js
@@ -1,4 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import store from '@/store'
+
 import HomeView from '../views/HomeView.vue'
 import Product from '../views/Product.vue'
 import About from '../views/AboutView.vue'
@@ -7,6 +9,7 @@ import Search from '../views/Search.vue'
 import Cart from '../views/Cart.vue'
 import SignUp from '../views/SignUp.vue'
 import LogIn from '../views/LogIn.vue'
+import MyAccount from '../views/MyAccount.vue'
 
 
 const routes = [
@@ -49,6 +52,14 @@ const routes = [
     path: '/log-in',
     name: 'LogIn',
     component: LogIn
+  },
+  {
+    path: '/my-account',
+    name: 'MyAccount',
+    component: MyAccount,
+    meta: {
+      requireLogin: true
+    }
   }
 ]
 
@@ -56,5 +67,11 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
-
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requireLogin) && !store.state.isAuthenticated) {
+    next({name: 'LogIn', query: { to: to.path }})
+  } else {
+    next()
+  }
+})
 export default router
--- vue_djacket/src/views/LogIn.vue
+++ vue_djacket/src/views/LogIn.vue
@@ -1,11 +1,10 @@
 <template>
-    <div class="page-sign-up">
+    <div class="page-log-in">
         <div class="columns">
             <div class="column is-4 is-offset-4">
                 <h1 class="title">Log in</h1>
 
                 <form @submit.prevent="submitForm">
-
                     <div class="field">
                         <label>Username</label>
                         <div class="control">
@@ -21,17 +20,18 @@
                     </div>
 
                     <div class="notification is-danger" v-if="errors.length">
-                        <p v-for="error in errors" v-bind:key="error"> {{ error }} </p>
+                        <p v-for="error in errors" v-bind:key="error">{{ error }}</p>
                     </div>
 
                     <div class="field">
                         <div class="control">
-                            <button class="button is-dark"> Log in</button>
+                            <button class="button is-dark">Log in</button>
                         </div>
                     </div>
-                    <hr>
-                    Or <router-link to="/log-in">Click here</router-link> to Sign-Up!
 
+                    <hr>
+
+                    Or <router-link to="/sign-up">click here</router-link> to sign up!
                 </form>
             </div>
         </div>
@@ -39,7 +39,7 @@
 </template>
 
 <script>
-import axios from 'axios';
+import axios from 'axios'
 
 export default {
     name: 'LogIn',
@@ -51,32 +51,33 @@ export default {
         }
     },
     mounted() {
-        document.title = 'Log in | Djacket'
+        document.title = 'Log In | Djackets'
     },
     methods: {
-         async submitForm() {
-            axios.defaults.headers.common['Authorization'] = ''
+        async submitForm() {
+            axios.defaults.headers.common["Authorization"] = ""
 
-            localStorage.removeItem('token')
+            localStorage.removeItem("token")
 
             const formData = {
                 username: this.username,
                 password: this.password
             }
+
             await axios
-                .post('/api/v1/token/login/', formData)
-                .then(resposne => {
-                    const token = resposne.data.auth_token
+                .post("/api/v1/token/login/", formData)
+                .then(response => {
+                    const token = response.data.auth_token
 
                     this.$store.commit('setToken', token)
+                    
+                    axios.defaults.headers.common["Authorization"] = "Token " + token
 
-                    axios.defaults.headers.common['Authorization'] = 'Token' + token
-
-                    localStorage.setItem('token', token)
+                    localStorage.setItem("token", token)
 
                     const toPath = this.$route.query.to || '/cart'
 
-                    this.$route.push(toPath)
+                    this.$router.push(toPath)
                 })
                 .catch(error => {
                     if (error.response) {
@@ -89,7 +90,7 @@ export default {
                         console.log(JSON.stringify(error))
                     }
                 })
-         }
-    },
+        }
+    }
 }
 </script>
